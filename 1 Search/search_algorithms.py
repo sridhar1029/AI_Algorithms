d980fceff74f5c7582a54e1e668d3a8c6f53d7ad
Sridhar <adhikarla2sridhar@yahoo.com>
2019-02-13 19:15:12 +0100

    Implemented UCS and A*, added the required Data Structures and some changes in UI

--- 1 Search/search_algorithms.py
+++ 1 Search/search_algorithms.py
@@ -1,69 +1,87 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 import numpy as np
-import utils
+from utils import *
 import time
 
 class AI_algorithms:
     def __init__(self, root):
         root.title("AI algorithms")
-        root.geometry("500x500")
+        root.geometry("500x520")
         root.resizable(width=False, height=False)
+        self.title_var = tk.StringVar(root)
+        self.title_var.set("Search Simulation")
+        title_app = tk.Label(root, textvariable=self.title_var, font=("Tahoma", 25, 'bold'))
+        title_app.pack(side=tk.TOP, fill=tk.X)
         self.create_canvas(420, 420)
-        self.btn_frame = Frame(root)
+        self.add_labels()
+        self.btn_frame = ttk.Frame(root)
         self.add_buttons()
         self.btn_frame.pack()
-        self.start = (0, 0)
+        self.start = (3, 12)
         self.goal = (20, 20)
-        self.arr = np.zeros((21, 21))
-        self.arr[self.start[0], self.start[1]] = 1
-        self.arr[self.goal[0], self.goal[1]] = 2
+        self.reset_arr()
         self.print_canvas()
+
+    def add_labels(self):
+        self.cost_var = tk.IntVar(root)
+        self.cost_var.set(0)
+        self.expanded_count = tk.IntVar(root)
+        self.expanded_count.set(0)
+        cost_lbl = tk.Label(root, text="Path Cost : ", font=("Tahoma", 10))
+        cost_label = tk.Label(root, textvariable=self.cost_var, font=("Tahoma", 10))
+        cost_lbl.pack(side=tk.LEFT, fill=tk.X)
+        cost_label.pack(side=tk.LEFT, fill=tk.X)
+        cexp_lbl = tk.Label(root, text="Nodes Expanded : ", font=("Tahoma", 10))
+        cexp_label = tk.Label(root, textvariable=self.expanded_count, font=("Tahoma", 10))
+        cexp_label.pack(side=tk.RIGHT, fill=tk.X)
+        cexp_lbl.pack(side=tk.RIGHT, fill=tk.X)
 
     def fill_canvas(self, event=None):
         print("Fill button clicked! The value of The dropdown is : ", self.var_alg.get())
         self.clear_canvas()
         if self.var_alg.get() == 'DFS':
             path = self.depthFirstSearch()
-        else:
+        elif self.var_alg.get() == 'BFS':
             path = self.breadthFirstSearch()
+        elif self.var_alg.get() == 'UCS':
+            path = self.uniformCostSearch()
+        else:
+            path = self.aStarSearch()
         self.print_canvas(path)
 
 
     def clear_canvas(self, event=None):
-        self.drawing_area.create_rectangle(0, 0, self.c_width, self.c_height, fill="black")
+        self.drawing_area.create_rectangle(0, 0, self.c_width, self.c_height, fill="white")
         self.reset_arr()
         self.print_canvas()
+        self.cost_var.set(0)
+        self.expanded_count.set(0)
         print("Cleared")
-
-    def check_canvas(self, event=None):
-        print("Check button clicked")
 
     def create_canvas(self, width, height):
         self.c_width = width
         self.c_height = height
-        self.drawing_area = Canvas(root,
+        self.drawing_area = tk.Canvas(root,
                                    width=self.c_width,
                                    height=self.c_height,
-                                   bg="black")
+                                   bg="white")
         self.drawing_area.pack()
 
     def add_buttons(self):
-        self.b_fill = ttk.Button(self.btn_frame, text="Fill", command=self.fill_canvas)
+        self.b_fill = ttk.Button(self.btn_frame, text="Search Goal", command=self.fill_canvas)
         self.b_clear = ttk.Button(self.btn_frame, text="Clear", command=self.clear_canvas)
-        self.b_check = ttk.Button(self.btn_frame, text="Check", command=self.check_canvas)
-        self.var_alg = StringVar(root)
+        self.var_alg = tk.StringVar(root)
         self.var_alg.set("DFS")
         self.options_menu = ttk.OptionMenu(self.btn_frame, self.var_alg, "DFS", "DFS", "BFS", "UCS", "A*")
-        self.b_fill.pack(side=LEFT)
-        self.b_clear.pack(side=LEFT)
-        self.b_check.pack(side=LEFT)
-        self.options_menu.pack(side=LEFT)
+        self.b_fill.pack(side=tk.LEFT)
+        self.b_clear.pack(side=tk.LEFT)
+        self.options_menu.pack(side=tk.LEFT)
 
     def reset_arr(self):
-        self.arr = np.zeros((21, 21))
-        self.arr[self.start[0], self.start[1]] = 1
-        self.arr[self.goal[0], self.goal[1]] = 2
+        self.arr = np.copy(search_space)
+        self.arr[self.start[0], self.start[1]] = 0
+        self.arr[self.goal[0], self.goal[1]] = 0
 
     def print_canvas(self, path=[]):
         start_x = 20 * self.start[1]
@@ -77,7 +95,20 @@ class AI_algorithms:
         end_y = 20 * (self.goal[0] + 1)
         self.drawing_area.create_rectangle(start_x, start_y, end_x, end_y, fill="green")
 
-        rows, cols = self.arr.shape
+        if path == []:
+            rows, cols = self.arr.shape
+            colors_arr = ["#000000", "#002730", "#013e4c", "#01657c", "#00a1c6"]
+            for i in range(rows):
+                for j in range(cols):
+                    if (i, j) != self.start and (i, j) != self.goal:
+                        ind = int(self.arr[i][j])
+                        start_x = 20 * j
+                        start_y = 20 * i
+                        end_x = 20 * (j + 1)
+                        end_y = 20 * (i + 1)
+                        self.drawing_area.create_rectangle(start_x, start_y, end_x, end_y, fill=colors_arr[ind])
+
+
         for i, j in path:
             start_x = 20 * j + 5
             start_y = 20 * i + 5
@@ -102,25 +133,25 @@ class AI_algorithms:
         if c > 0:
             cl = c - 1
             s_left = (r, cl)
-            suc.append((s_left, [s_left], 0))
+            suc.append((s_left, [s_left], int(self.arr[r][cl]) + 1))
 
         #right
         if c < 20:
             cr = c + 1
             s_right = (r, cr)
-            suc.append((s_right, [s_right], 0))
+            suc.append((s_right, [s_right], int(self.arr[r][cr]) + 1))
 
         #top
         if r > 0:
             rt = r - 1
             s_top = (rt, c)
-            suc.append((s_top, [s_top], 0))
+            suc.append((s_top, [s_top], int(self.arr[rt][c]) + 1))
 
         #bottom
         if r < 20:
-            rt = r + 1
-            s_bot = (rt, c)
-            suc.append((s_bot, [s_bot], 0))
+            rb = r + 1
+            s_bot = (rb, c)
+            suc.append((s_bot, [s_bot], int(self.arr[rb][c]) + 1))
 
         return suc
 
@@ -141,7 +172,7 @@ class AI_algorithms:
 
 
     def depthFirstSearch(self):
-        fringe = utils.Stack()
+        fringe = Stack()
         start = self.getStartState()
         fringe.push((start, [], 0))
         visited = set([])
@@ -152,16 +183,19 @@ class AI_algorithms:
                 if self.isGoalState(state):
                     print(state, "Found GOAL")
                     print(path)
+                    print(cost)
+                    self.expanded_count.set(len(visited))
+                    self.cost_var.set(cost)
                     return path
                 else:
                     for suc in self.getSuccessors(state):
                         suc_state, suc_dir, suc_cost = suc
-                        fringe.push((suc_state, path + suc_dir, 0))
+                        fringe.push((suc_state, path + suc_dir, cost + suc_cost))
                 self.mark_visited(state)
         return []
 
     def breadthFirstSearch(self):
-        fringe = utils.Queue()
+        fringe = Queue()
         start = self.getStartState()
         fringe.push((start, [], 0))
         visited = set([])
@@ -172,15 +206,73 @@ class AI_algorithms:
                 if self.isGoalState(state):
                     print(state, "Found GOAL")
                     print(path)
+                    print(cost)
+                    self.expanded_count.set(len(visited))
+                    self.cost_var.set(cost)
                     return path
                 else:
                     for suc in self.getSuccessors(state):
                         suc_state, suc_dir, suc_cost = suc
-                        fringe.push((suc_state, path + suc_dir, 0))
+                        fringe.push((suc_state, path + suc_dir, cost + suc_cost))
+                self.mark_visited(state)
+        return []
+
+    def uniformCostSearch(self):
+        print("Starting UCS")
+        fringe = PriorityQueue()
+        start = self.getStartState()
+        fringe.push((start, [], 0), 0)
+        visited = set([])
+        while(fringe.isEmpty() == False):
+            state, path, cost = fringe.pop()
+            if state not in visited:
+                visited.add(state)
+                if self.isGoalState(state):
+                    print(state, "Found GOAL")
+                    print(path)
+                    print(cost)
+                    self.expanded_count.set(len(visited))
+                    self.cost_var.set(cost)
+                    return path
+                else:
+                    for suc in self.getSuccessors(state):
+                        suc_state, suc_dir, suc_cost = suc
+                        fringe.push((suc_state, path + suc_dir, cost + suc_cost), cost + suc_cost)
+                self.mark_visited(state)
+        return []
+
+    def manhattanDist(self, state):
+        dis = np.abs(state[0] - self.goal[0]) + np.abs(state[1] - self.goal[1])
+        return dis
+
+    def aStarSearch(self, heuristic=None):
+        if heuristic == None:
+            heuristic = self.manhattanDist
+        fringe = PriorityQueue()
+        start = self.getStartState()
+        fringe.push((start, [], 0), 0 + heuristic(start))
+        visited = set([])
+        while (fringe.isEmpty() == False):
+            state, path, cost = fringe.pop()
+            if state not in visited:
+                visited.add(state)
+                if self.isGoalState(state):
+                    print(state, "Found GOAL")
+                    print(path)
+                    print(cost)
+                    self.expanded_count.set(len(visited))
+                    self.cost_var.set(cost)
+                    return path
+                else:
+                    for suc in self.getSuccessors(state):
+                        suc_state, suc_dir, suc_cost = suc
+                        cum_cost = cost + suc_cost
+                        f = cum_cost + heuristic(suc_state)
+                        fringe.push((suc_state, path + suc_dir, cum_cost), f)
                 self.mark_visited(state)
         return []
 
 
-root = Tk();
+root = tk.Tk()
 app = AI_algorithms(root)
 root.mainloop()
